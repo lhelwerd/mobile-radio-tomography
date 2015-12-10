4f8012c6e1fd9eae9d85f793ceafbdfdb9dd1e20
Leon Helwerda <leon.helwerda@planet.nl>
2015-12-10 14:22:31 +0100

    Simulation: Use starting translations per scene
    Determine a translation based on the scene name and add it to the
    arguments. Fix moving files again.

--- simulations.py
+++ simulations.py
@@ -5,10 +5,36 @@ import itertools
 import subprocess
 from collections import OrderedDict
 
+def format_arg(key, value):
+    pair = []
+    if key == "scenefile":
+        scenes = {
+            "castle": {
+                "filename": "tests/vrml/castle.wrl",
+                "translation": [0, -40, 0]
+            },
+            "trees_river": {
+                "filename": "tests/vrml/trees_river.wrl",
+                "translation": [0, 0, 2.5]
+            },
+            "deranged_house": {
+                "filename": "tests/vrml/deranged_house.wrl",
+                "translation": [4.1, 6.25, 0]
+            }
+        }
+        if value not in scenes:
+            raise ValueError("Incorrect scene")
+
+        pair += ["--translation"] + [str(t) for t in scenes[value]["translation"]]
+        value = scenes[value]["filename"]
+
+    pair += ["--{}".format(key.replace('_','-')), str(value)]
+    return pair
+
 def main(argv):
     permutations = OrderedDict([
         ("mission_class", ["Mission_Square", "Mission_Browse", "Mission_Search", "Mission_Pathfind"]),
-        ("scenefile", ["tests/vrml/castle.wrl", "tests/vrml/trees_river.wrl", "tests/vrml/deranged_house.wrl"]),
+        ("scenefile", ["castle", "trees_river", "deranged_house"]),
         ("closeness", [0.1, 2.0]),
         ("padding", [0.1, 4.0]),
         ("resolution", [1, 5])
@@ -18,7 +44,8 @@ def main(argv):
         path = '+'.join(["{}-{}".format(k,v.replace('/',',') if isinstance(v,str) else v) for (k,v) in arguments.iteritems()])
         print(path)
 
-        args = list(itertools.chain(*[["--{}".format(k.replace('_','-')), str(v)] for (k,v) in arguments.iteritems()]))
+        arg_pairs = [format_arg(k,v) for (k,v) in arguments.iteritems()]
+        args = list(itertools.chain(*arg_pairs))
 
         with open("output.log","w") as output:
             with open("error.log","w") as error:
@@ -30,7 +57,7 @@ def main(argv):
 
         files = ["output.log", "error.log", "plot.eps", "map.npy"]
         for file in files:
-            if os.path.exists(path):
+            if os.path.exists(file):
                 shutil.move(file, path + "/" + file)
 
 if __name__ == "__main__":
